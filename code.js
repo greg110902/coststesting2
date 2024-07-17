5310f372794f546419ac7d5e6170b686ca5bf045
greg110902 <gwgh1g21@soton.ac.uk>
2024-07-17 14:34:37 +0100

    a

--- code.js
+++ code.js
@@ -1,11 +1,22 @@
+
+
+import { request } from 'http'
 import onRequest from '/functions/submit'
 
 async function doIt() {
-    payer = 'greg'
-    affecting = ['alex', 'leo', 'harry']
-    value = 100
-
-    onRequest(payer, value, affecting)
+    fetch('https://coststesting2.pages.dev/functions/submit')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch Error:', error);
+  });
 }
 
 document.getElementById('button').addEventListener('click', doIt)
--- functions/submit.js
+++ functions/submit.js
@@ -1,4 +1,7 @@
-export async function onRequest(payer, value, affecting){
+export async function onRequest(){
+    value = 100
+    payer = 'greg'
+    affecting = ['alex', 'leo', 'harry']
     const owedByAffected = value / (affecting.length + 1)
     const owedToPayer = value * (affecting.length) / (affecting.length + 1)
     for (const payee of affecting){
